5af15656f1112135113cd0d615c4ebe9ec8f1c30
pompydev <hello@pompy.dev>
2026-05-02 17:14:20 +0900

    add hongik logo to footer

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,7 +1,23 @@
+import hongik_logotype from "./hongik_logotype.svg"
+import Image from "next/image"
+
 export default function Footer() {
   return (
-    <footer className="bg-hongik-black py-6 text-center text-sm text-hongik-white selection:bg-hongik-white selection:text-hongik-black!">
-      © {new Date().getFullYear()} TRIA Lab. All rights reserved.
+    <footer className="flex h-24 items-center justify-center bg-hongik-black text-center text-sm text-hongik-white selection:bg-hongik-white selection:text-hongik-black!">
+      <div className="flex size-full max-w-350 items-center justify-between">
+        <div className="flex items-center">
+          <span className="text-xl font-bold text-hongik-white">[LOGO] TRIA LAB</span>
+          <span className="mx-2 text-xl font-bold">@</span>
+          <Image
+            src={hongik_logotype}
+            alt="Hongik logotype"
+            width={321}
+            height={98}
+            className="aspect-321/98 w-28"
+          />
+        </div>
+        <div>© {new Date().getFullYear()} TRIA Lab. All rights reserved.</div>
+      </div>
     </footer>
   )
 }
