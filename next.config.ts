560040b69898175e6224636465764d897e575f5c
pompydev <hello@pompy.dev>
2026-05-02 16:43:40 +0900

    add more information to teams

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-
+  images: { unoptimized: true },
   // https://nextjs.org/docs/app/guides/static-exports
   output: "export",
   trailingSlash: true,
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -2,11 +2,12 @@ import { A } from "@/components/Link"
 import Markdown from "@/components/Markdown"
 import getContent from "@/lib/content/getContent"
 import { pageTitle } from "@/lib/utils"
-import { SiGooglescholar } from "@icons-pack/react-simple-icons"
+import { SiGooglescholar, SiGithub } from "@icons-pack/react-simple-icons"
 import fs from "fs"
 import { load } from "js-yaml"
-import { Mail } from "lucide-react"
+import { Mail, User } from "lucide-react"
 import type { Metadata } from "next"
+import Image from "next/image"
 import { z } from "zod"
 
 export const metadata: Metadata = {
@@ -14,10 +15,17 @@ export const metadata: Metadata = {
 }
 
 const studentSchema = z.object({
-  name: z.string().min(1),
+  nameKo: z.string(),
+  nameEn: z.string().optional(),
+  department: z.string().optional(),
+  researchArea: z.string().optional(),
+  profilePicture: z.string().optional(),
+  github: z.url().optional(),
 })
 
-const professorSchema = studentSchema.extend({
+const professorSchema = z.object({
+  nameKo: z.string(),
+  nameEn: z.string().optional(),
   email: z.email(),
   googleScholar: z.url().optional(),
 })
@@ -46,9 +54,9 @@ export default function Team() {
       <section className="mb-12">
         <h2 className="mb-6 text-2xl font-semibold">Professors</h2>
         <div className="grid gap-6">
-          {professors.map(({ name, email, googleScholar }) => (
+          {professors.map(({ nameKo, email, googleScholar }) => (
             <div key={email} className="rounded-lg border p-6">
-              <h3 className="inline text-xl font-semibold">{name}</h3>{" "}
+              <h3 className="inline text-xl font-semibold">{nameKo}</h3>{" "}
               {googleScholar && (
                 <A href={googleScholar} title="Google Scholar" noIcon>
                   <SiGooglescholar />
@@ -67,9 +75,43 @@ export default function Team() {
       <section className="mb-12">
         <h2 className="mb-6 text-2xl font-semibold">Students</h2>
         <div className="grid gap-6 md:grid-cols-2">
-          {students.map(({ name }) => (
-            <div key={name} className="rounded-lg border p-6">
-              <h3 className="text-xl font-semibold">{name}</h3>
+          {students.map(({ nameKo, department, researchArea, github }) => (
+            <div key={nameKo} className="rounded-lg border p-6">
+              {fs.existsSync(`public/team/${nameKo}.png`) ? (
+                <Image
+                  src={`/team/${nameKo}.png`}
+                  alt={nameKo}
+                  width={1000}
+                  height={750}
+                  className="mb-4 aspect-3/4"
+                />
+              ) : (
+                <div className="mb-4 flex aspect-3/4 items-center justify-center">
+                  <User className="size-full text-gray-200" strokeWidth={0.5} />
+                </div>
+              )}
+
+              <div className="mb-4 flex h-10 items-center">
+                <h3 className="inline text-xl font-semibold">{nameKo}</h3>
+                {github && (
+                  <A href={github} title="Github" noIcon className="ml-2 inline-block">
+                    <SiGithub />
+                  </A>
+                )}
+              </div>
+
+              {department && (
+                <>
+                  <span className="mb-2 font-bold">Department:</span> <span>{department}</span>
+                  <br />
+                </>
+              )}
+
+              {researchArea && (
+                <>
+                  <span className="font-bold">Research Area:</span> <span>{researchArea}</span>
+                </>
+              )}
             </div>
           ))}
         </div>
