08bd21703f127fcb53a530567507b4b57c5c13c2
pompydev <hello@pompy.dev>
2026-05-07 14:32:48 +0900

    match footer padding with navbar

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function Footer() {
   return (
     <footer className="flex h-24 items-center justify-center bg-hongik-black text-center text-sm text-hongik-white selection:bg-hongik-white selection:text-hongik-black!">
-      <div className="flex size-full max-w-350 items-center justify-between">
+      <div className="flex size-full max-w-350 items-center justify-between px-6">
         <div className="flex items-center">
           <span className="text-xl font-bold text-hongik-white">[LOGO] TRIA LAB</span>
           <span className="mx-2 text-xl font-bold">@</span>
