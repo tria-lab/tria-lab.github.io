b6cbca1e006b31d30775840bec6104e3cd959740
pompydev <hello@pompy.dev>
2026-07-29 18:55:01 +0900

    redirect without client-side javascript

--- src/app/(redirect)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "../globals.css"
-
-export default function RedirectLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
-}
