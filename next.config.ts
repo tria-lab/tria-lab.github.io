8231ebf01060845da2030821dc895e4313002e31
pompydev <hello@pompy.dev>
2026-07-29 11:36:32 +0900

    fix memory leak in dev server

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  experimental: { globalNotFound: true },
+  experimental: {
+    globalNotFound: true,
+    turbopackServerFastRefresh: false,
+  },
   reactCompiler: true,
   images: { unoptimized: true },
   // https://nextjs.org/docs/app/guides/static-exports
